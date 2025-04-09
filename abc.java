bfde985e0c1e481692491cba5c8568f6428fd4d4
saikrishna878 <nanaboinasaikrishna555@gmail.com>
2025-04-09 22:15:16 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,8 +1,8 @@
 public class abc{
 public static void main(String...args)
 {
-System.out.println("saikrishna");
+System.out.println("sky");
 System.out.println("saikrishnayadav");
-System.out.println("Divya jadav");
+System.out.println("vijaysai");
 }
 }
